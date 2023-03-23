f1f735eb656a47573fe9122eac26fd59bf9f4e67
Jon Brighton <brightonsbox@hotmail.com>
2023-03-23 22:21:09 +0000

    Adding some more example data to the API

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,58 @@
 const typeDefs = `#graphql
 
+  type Education {
+    yearFrom: Int
+    yearTo: Int
+    establishment: String
+    courseTitle: String
+    grade: String
+  }
+
+  type Skill {
+    name: String
+    category: String
+  }
+
   type JobRole {
     organisation: String
     title: String
   }
 
   type Query {
+    firstName: String
+    lastName: String
+    educationHistory: [Education]
+    skills: [Skill]
     roles: [JobRole]
   }
 `
+
+const hardCodedEducationHistory = [
+  {
+    yearFrom: 2008,
+    yearTo: 2012,
+    establishment: 'University of Cambridge, Girton College',
+    courseTitle: 'MSci, MA (Cantab) Natural Sciences: Experimental and Theoretical Physics',
+    grade: '2.1',
+  }
+]
+
+const hardCodedSkills = [
+  {
+    category: 'Languages / Frameworks / Tools',
+    skills: [
+      'Node.js',
+      'Express.js',
+      'Nunjucks',
+      'React.js',
+      'Redux.js',
+    ]
+  },
+  {
+    category: 'Scripting Languages',
+    skills: [ 'Bash', 'Python' ],
+  }
+]
 
 const hardCodedRoles = [
   {
@@ -23,6 +67,10 @@ const hardCodedRoles = [
 
 const resolvers = {
   Query: {
+    firstName: () => 'Jonathan',
+    lastName: () => 'Brighton',
+    educationHistory: () => hardCodedEducationHistory,
+    skills: () => hardCodedSkills.flatMap(category => category.skills.map(skill => ({ name: skill, category: category.category }))),
     roles: () => hardCodedRoles,
   },
 }
